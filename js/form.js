d6b59e9ebcd40efe8a148e7bb06890faef8bc052
Thata <126881634+Thanaha@users.noreply.github.com>
2023-10-11 08:24:53 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,25 +1,47 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
+botaoAdicionar.addEventListener("click", function (event){
     event.preventDefault();
 
-var form = document.querySelector("#form-adiciona");
-var paciente = obtemPacienteDoFormulario(form);
+    var form = document.querySelector("#form-adiciona");
+    var paciente = obtemPacienteDoFormulario(form);
 
-var pacienteTr = montaTr(paciente);
+    var pacienteTr = montaTr(paciente);
 
-if(!validaPaciente(paciente)){
-    console.log("Paciente inválido");
-    return;
+    var erros = validaPaciente(paciente);
+    if(erros.length > 0){
+        exibeMensagemDeErro(erros);
+        return;
+    }
+
+    adicionaPacienteNaTabela(paciente);
+
+    form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro")
+    mensagensErro.innerHTML = "";
+});
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 }
 
-// adicionando o paciente na tabela
-var tabela = document.querySelector("#tabela-pacientes");
+function exibeMensagemDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    
+    erros.forEach(function(erros) {
+        var li = document.createElement("li");
+        li.textContent = erros;
+        ul.appendChild(li);
+    });
+}
 
-tabela.appendChild(pacienteTr);
-
-form.reset();
-
-});
+   function adicionaPacienteNaTabela(paciente) {
+        var pacienteTr = montaTr(paciente);
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
+    }
 
 function obtemPacienteDoFormulario(form){
 
@@ -28,10 +50,10 @@ function obtemPacienteDoFormulario(form){
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
-        imc: calculaImc(form.peso.value, form.altura.value)
+        imc: calculaImc(form.peso.value, form.altura.value,)
     }
-
-    return paciente; 
+    
+    return paciente;
 }
 
 function montaTr(paciente){
@@ -43,11 +65,9 @@ function montaTr(paciente){
     pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
     pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
     pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
-   
 
     return pacienteTr;
 }
-
 
 function montaTd(dado,classe){
     var td = document.createElement("td");
@@ -56,15 +76,32 @@ function montaTd(dado,classe){
     return td;
 }
 
-
 function validaPaciente(paciente){
-    if(validaPeso(paciente.peso)){
-        return true;
-    }else{
-        return "O Peso é inválido";
+    
+    var erros = [];
+    
+    if( paciente.nome.length == 0){
+        erros.push("O nome não pode ser nulo")
     }
+
+    if(!validaPeso(paciente.peso)){
+        erros.push("Peso é inválido!")
+    }
+    if(!validaAltura(paciente.altura)){
+        erros.push("Altura é inválida!");
+    }
+
+    if( paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser nulo")
+    }
+
+    if( paciente.peso.length == 0){
+        erros.push("O peso não pode ser nulo")
+    }
+
+    if( paciente.altura.length == 0){
+        erros.push("A altura não pode ser nula")
+    }
+
+    return erros;
 }
-
-
-
-//
